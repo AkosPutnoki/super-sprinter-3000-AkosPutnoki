d67ab0635971615121b8909568015019be4eacc4
Akos Putnoki <putnokiakos@gmail.com>
2017-08-10 16:42:54 +0200

    form and list basics created

--- server.py
+++ server.py
@@ -7,9 +7,13 @@ def index():
     return render_template("list.html")
 
 
+@app.route("/story")
+def story():
+    return render_template("form.html")
+
 
 if __name__=="__main__":
-    app.secret_key = "hurkaleves"
+    app.secret_key = "kekekeke"
     app.run(
         debug=True,
         port=5000
